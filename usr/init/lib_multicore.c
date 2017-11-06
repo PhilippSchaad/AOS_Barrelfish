a3ffa74030cfd6b57968af1e134d7662b41b82ea
Philipp Schaad <schaad.phil@gmail.com>
2017-11-06 21:33:25 +0100

    Added memory-allocation part for multicore

--- usr/init/lib_multicore.c
+++ usr/init/lib_multicore.c
@@ -1,24 +1,29 @@
 #include "lib_multicore.h"
 
-static errval_t core_allocate_memory(void) {
+static errval_t core_allocate_memory(struct arm_core_data **ret_core_data,
+                                     coreid_t core_id,
+                                     coreid_t current_core_id) {
     size_t retsize;
 
     // I: Allocate a new KCB.
     struct capref kcb_ram, kcb_frame;
+    struct frame_identity kcb_frame_identity;
     CHECK(ram_alloc(&kcb_ram, OBJSIZE_KCB));
     CHECK(slot_alloc(&kcb_frame));
     CHECK(cap_retype(kcb_frame, kcb_ram, 0, ObjType_KernelControlBlock,
                      OBJSIZE_KCB, 1));
+    CHECK(frame_identify(kcb_frame, &kcb_frame_identity));
 
     // II: Allocate coreboot's core_data struct.
-    struct capref core_data_memframe;
+    struct capref core_data_frame;
     struct arm_core_data *core_data;
-    CHECK(frame_alloc(&core_data_memframe, OBJSIZE_KCB, &retsize));
-    CHECK(invoke_kcb_clone(kcb_frame, core_data_memframe));
+    CHECK(frame_alloc(&core_data_frame, OBJSIZE_KCB, &retsize));
+    CHECK(invoke_kcb_clone(kcb_frame, core_data_frame));
     CHECK(paging_map_frame(get_current_paging_state(), (void *) &core_data,
-                           retsize, core_data_memframe, NULL, NULL));
+                           retsize, core_data_frame, NULL, NULL));
 
     // III: Allocate memory to load coreboot's relocatable segment. TODO
+    // Idk what exactly this has to be?...
 
     // IV: Allocate space to load the init process.
     struct capref init_frame;
@@ -26,21 +31,49 @@ static errval_t core_allocate_memory(void) {
     CHECK(frame_alloc(&init_frame, ARM_CORE_DATA_PAGES * BASE_PAGE_SIZE,
                       &retsize));
     CHECK(frame_identify(init_frame, &init_frame_identity));
+
+    // V: Identify the provided urpc frame (no need to alloc it, yay!)
+    struct frame_identity urpc_frame_identity;
+    CHECK(frame_identify(cap_urpc, &urpc_frame_identity));
+
+    // Fill in the core_data struct.
+    core_data->kcb = kcb_frame_identity.base;
     core_data->memory_base_start = init_frame_identity.base;
     core_data->memory_bytes = init_frame_identity.bytes;
+    core_data->urpc_frame_base = urpc_frame_identity.base;
+    core_data->urpc_frame_size = urpc_frame_identity.bytes;
+    core_data->dst_core_id = core_id;
+    core_data->src_core_id = current_core_id;
 
-    // V: Allocate an URPC frame. TODO
+    *ret_core_data = core_data;
 
-    return LIB_ERR_NOT_IMPLEMENTED;
+    return SYS_ERR_OK;
 }
 
-static errval_t load_and_relocate_driver(void)
+static errval_t load_and_relocate_driver(struct bootinfo *bootinfo,
+                                         struct arm_core_data *core_data)
 {
-    return LIB_ERR_NOT_IMPLEMENTED;
-}
+    struct mem_region *init_module = multiboot_find_module(bootinfo, "init");
+    if (!init_module) {
+        DBG(ERR, "Failed to load init module\n");
+        return SPAWN_ERR_FIND_MODULE;
+    }
 
-static errval_t fill_core_data(void) {
-    return LIB_ERR_NOT_IMPLEMENTED;
+    struct multiboot_modinfo module_info = {
+        .mod_start = init_module->mr_base,
+        .mod_end = init_module->mr_base + init_module->mrmod_size,
+        .string = init_module->mrmod_data,
+        .reserved = 0
+    };
+    core_data->monitor_module = module_info;
+
+    core_data->init_name[0] = 'i';
+    core_data->init_name[1] = 'n';
+    core_data->init_name[2] = 'i';
+    core_data->init_name[3] = 't';
+    core_data->init_name[4] = '\0';
+
+    return SYS_ERR_OK;
 }
 
 static errval_t clean_cache(void) {
@@ -51,7 +84,8 @@ static errval_t invoke_kernel_cap(void) {
     return LIB_ERR_NOT_IMPLEMENTED;
 }
 
-errval_t wake_core(coreid_t core_id, coreid_t current_core_id)
+errval_t wake_core(coreid_t core_id, coreid_t current_core_id,
+                   struct bootinfo *bootinfo)
 {
     // If the core id is 0, this core is already up and does not need to be
     // woken up.
@@ -61,11 +95,11 @@ errval_t wake_core(coreid_t core_id, coreid_t current_core_id)
     // awake too.
     assert(core_id != current_core_id);
 
-    CHECK(core_allocate_memory());
+    struct arm_core_data *core_data;
 
-    CHECK(load_and_relocate_driver());
+    CHECK(core_allocate_memory(&core_data, core_id, current_core_id));
 
-    CHECK(fill_core_data());
+    CHECK(load_and_relocate_driver(bootinfo, core_data));
 
     CHECK(clean_cache());
 
--- usr/init/lib_multicore.h
+++ usr/init/lib_multicore.h
@@ -5,7 +5,9 @@
 #include <aos/coreboot.h>
 #include <aos/kernel_cap_invocations.h>
 #include <barrelfish_kpi/arm_core_data.h>
+#include <spawn/multiboot.h>
 
-errval_t wake_core(coreid_t core_id, coreid_t current_core_id);
+errval_t wake_core(coreid_t core_id, coreid_t current_core_id,
+                   struct bootinfo *bootinfo);
 
 #endif /* LIB_MULTICORE_H */
